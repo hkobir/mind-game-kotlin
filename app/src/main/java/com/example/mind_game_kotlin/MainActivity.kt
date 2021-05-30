c7e53bfad65261198137ea6be92e09df67f1be88
hkobir <hkobir10@gmail.com>
2021-05-30 23:41:08 +0600

    setup game board is done

--- app/src/main/java/com/example/mind_game_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/mind_game_kotlin/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.mind_game_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mind_game_kotlin.models.BoardSize
+import com.example.mind_game_kotlin.utils.DEFAULT_ICONS
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var boardRv: RecyclerView
+    private lateinit var pairsTv: TextView
+    private lateinit var movesTv: TextView
+
+    private var boardSize: BoardSize = BoardSize.EASY
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //init
+        boardRv = findViewById(R.id.boardRV)
+        pairsTv = findViewById(R.id.pairsTV)
+        movesTv = findViewById(R.id.movesTV)
+
+        val chooseImages = DEFAULT_ICONS.shuffled().take(boardSize.getNumPairs())
+        val randomizedImage = (chooseImages+chooseImages).shuffled()
+
+        boardRv.layoutManager = GridLayoutManager(this, boardSize.getWidth())
+        boardRv.setHasFixedSize(true)
+        boardRv.adapter = MemoryAdapter(this, boardSize,randomizedImage)
     }
 }
---
+++ app/src/main/java/com/example/mind_game_kotlin/MemoryAdapter.kt
@@ -0,0 +1,56 @@
+package com.example.mind_game_kotlin
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mind_game_kotlin.models.BoardSize
+import kotlin.math.min
+
+class MemoryAdapter(
+    private val context: Context,
+    private val boardSize: BoardSize,
+    private val cardImages: List<Int>
+) :
+    RecyclerView.Adapter<MemoryAdapter.ViewHolder>() {
+    companion object {
+        private const val MARGIN_SIZE = 10
+        private const val TAG = "MemoryAdapter"
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+//        divide all memory cards to equal size
+        val cardWidth = parent.width / boardSize.getWidth() - (2 * MARGIN_SIZE)
+        val cardHeight = parent.height / boardSize.getHeight() - (2 * MARGIN_SIZE)
+        val cardSideLength = min(cardWidth, cardHeight)
+
+        val view = LayoutInflater.from(context).inflate(R.layout.item_memory_card, parent, false)
+        val layoutParams =
+            view.findViewById<CardView>(R.id.cardView).layoutParams as ViewGroup.MarginLayoutParams
+//        Log.d(TAG+ ": " + cardWidth + ", " + cardHeight + ", " + cardSideLength)
+        layoutParams.width = cardSideLength
+        layoutParams.height = cardSideLength
+        layoutParams.setMargins(MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    override fun getItemCount(): Int = boardSize.numCards
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val cardButton = itemView.findViewById<ImageButton>(R.id.imageButton)
+        fun bind(position: Int) {
+            cardButton.setImageResource(cardImages[position])
+            cardButton.setOnClickListener {
+
+            }
+        }
+    }
+}
